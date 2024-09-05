f3a3d3786c09bb645250840c6f59259c671623b2
kim yunjung <1106qwe@hanmail.net>
2024-09-05 20:15:07 +0900

    spring / react

--- todo/src/main/java/org/test/demo/todo/controller/MainController.java
+++ todo/src/main/java/org/test/demo/todo/controller/MainController.java
@@ -6,18 +6,27 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @Slf4j
 @RequiredArgsConstructor
+@RestController
 @Controller
 public class MainController {
     //http://localhost:8080/swagger-ui/index.html
 
     //http://localhost:8080/
     @GetMapping("/")
+    @ResponseBody
     public String root(){
         return "index";
+    }
+
+    //http://localhost:8080/api/test
+    @GetMapping("/api/test")
+    public String reactRoot() {
+        return "react index";
     }
 }
