5b30e40730d9a20f8d63b77dbf1cef811bb6cab1
kim yunjung <1106qwe@hanmail.net>
2024-09-06 15:08:56 +0900

    spring / react / swagger

--- todo/src/main/java/org/test/demo/todo/env/EnvConfig.java
+++
@@ -1,9 +0,0 @@
-package org.test.demo.todo.env;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-
-@Configuration
-@PropertySource("classpath:properties/env.properties")
-public class EnvConfig {
-}
